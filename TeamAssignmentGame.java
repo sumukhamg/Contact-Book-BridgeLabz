e6e2fdf970ee0ab90cb21d79e45dca01a57cb834
SumukhaMG <sumukhamg77266@gmail.com>
2025-08-04 15:40:02 +0530

    Modeefied TeamAssignmeentGame

--- TeamAssignmentGame.java
+++ TeamAssignmentGame.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class TeamAssignmentGame {
     public static void main(String args[]) {
-        int[] teams = { 2, 45, 54, 12, 67, 98 };
+        int[] teams = { 10, 20, 30, 40, 50, 60 };
         Arrays.sort(teams);
         int sum1 = 0, sum2 = 0;
         int[] team1 = new int[teams.length / 2];
@@ -19,11 +19,9 @@ public class TeamAssignmentGame {
             }
         }
 
-        if (sum1 <= sum2) {
-            int t = teams[0];
-            team1[0] = team2[0];
-            team2[0] = t;
-        }
+        int t = teams[0];
+        team1[0] = team2[0];
+        team2[0] = t;
 
         System.out.println(Arrays.toString(team1));
         System.out.println(Arrays.toString(team2));
