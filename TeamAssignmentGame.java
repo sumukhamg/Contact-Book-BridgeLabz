b129de70c7fd408c1ebc9c74a764977d9dfc328e
SumukhaMG <sumukhamg77266@gmail.com>
2025-08-05 15:59:45 +0530

    added day 2 ppractice sets

--- TeamAssignmentGame.java
+++ TeamAssignmentGame.java
@@ -2,29 +2,29 @@ import java.util.Arrays;
 
 public class TeamAssignmentGame {
     public static void main(String args[]) {
-        int[] teams = { 10, 20, 30, 40, 50, 60 };
-        Arrays.sort(teams);
-        int sum1 = 0, sum2 = 0;
-        int[] team1 = new int[teams.length / 2];
-        int[] team2 = new int[teams.length / 2];
+        int[] arr = { 10, 20, 30, 40 };
+        Arrays.sort(arr);
+        int[] teamA = new int[arr.length / 2];
+        int[] teamB = new int[arr.length / 2];
+        int sumA = 0, sumB = 0;
         int j = 0, k = 0;
 
-        for (int i = 0; i < teams.length; i++) {
-            if (i % 2 == 0) {
-                sum1 += teams[i];
-                team1[j++] = teams[i];
-            } else {
-                sum2 += teams[i];
-                team2[k++] = teams[i];
+        for (int i = 0; i < arr.length; i++) {
+            if (sumA <= sumB) {
+                teamA[j++] = arr[i];
+                sumA += arr[i];
             }
+
+            else {
+                teamB[k++] = arr[i];
+                sumB += arr[i];
+
+            }
+
         }
 
-        int t = teams[0];
-        team1[0] = team2[0];
-        team2[0] = t;
-
-        System.out.println(Arrays.toString(team1));
-        System.out.println(Arrays.toString(team2));
+        System.out.println(Arrays.toString(teamA));
+        System.out.println(Arrays.toString(teamB));
     }
 
 }
